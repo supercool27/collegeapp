c4e1210987fc52cd47cdd91db2dad31504a93813
supercool27 <manojpali@live.com>
2022-05-05 12:47:39 +0530

    removed unwanted code

--- SplashScreen.js
+++ SplashScreen.js
@@ -4,15 +4,10 @@ import DrawerNavigationRoutes from './DrawerNavigationRoutes';
 import AsyncStorage from '@react-native-community/async-storage';
 import {useNavigation} from '@react-navigation/native';
 const SplashScreen = ({navigation}) => {
- // const navigation1 = useNavigation();
   const [animating, setAnimating] = useState(true);
-  
   useEffect(() => {
     setTimeout(() => {
       setAnimating(true);
-      //Check if user_id is set or not
-      //If not then send for Authentication
-      //else send to Home Screen
       AsyncStorage.getItem('Otp_details').then((value) =>
         navigation.replace(
           value === null ? 'Auth' : 'DrawerNavigationRoutes'
@@ -20,31 +15,6 @@ const SplashScreen = ({navigation}) => {
       );
     }, 5000);
   }, []);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setAnimating(false);
-  //     //Check if user_id is set or not
-  //     //If not then send for Authentication
-  //     //else send to Home Screen
-  //     value = AsyncStorage.getItem('Otp_details');
-  //     console.log(AsyncStorage.getItem("Otp_details"));
-  //     console.log(AsyncStorage.getItem("Otp_details").otp_value);
-  //     console.log(AsyncStorage.getItem("Otp_details").student_id);
-  //     if(AsyncStorage.getItem("Otp_details").otp_value===null){
-  //       console.log('-------------------------------------------Auth-------------------------------------------');
-  //       navigation.replace('Auth');
-  //     } 
-  //     else{ 
-  //       console.log('------------------------------------DrawerNavigationRoutes--------------------------------');
-  //       navigation.replace('DrawerNavigationRoutes');
-  //       // navigation.navigate('DrawerNavigationRoutes', {
-  //       //   screen: 'The name of the screen you want to go inside DraweNavigatonRoutes stack navigator',
-  //       // });
-  //     }
-  //   }, 5000);
-  // }, []);
-
   return (
     <View style={styles.container}>
       <Image
